baa0b51c4c84a5724b6106a5ca259f629126bb55
Dante <lcdantechen@gmail.com>
2016-07-17 20:55:03 -0400

    update

--- word.js
+++ word.js
@@ -5,7 +5,7 @@ var radonNumber;
 var computerChoice;
 var answerWord;
 var curWord = [];
-var guessesLeft = 0;
+var guessesLeft = 15;
 var guessesSoFar = [];
 
 
@@ -24,46 +24,83 @@ var Check = function(guess) {
     randomNumber =  Math.floor(Math.random() * random.randomWords.length);
     computerChoice = random.randomWords[randomNumber].word;
     answerWord = computerChoice.split('');
-    guessesLeft = answerWord.length + 5;
    /* console.log(answerWord);*/
-  	 for (var i =0;i<answerWord.length;i++) {
-		   curWord.push('_');
-	   };
-	 /* console.log(curWord);*/
+	  if (curWord.indexOf("_")>=0) {
 
-	  if (curWord.indexOf(guess)>=0) {
-	  	console.log('You guessed this letter already!')
-	  } else if (answerWord.indexOf(guess)>=0) {
+	  	   if (curWord.indexOf(guess)>=0) {
+			  	console.log('You guessed this letter already!')
+			  } else if (answerWord.indexOf(guess)>=0) {
 
-			   for (var i =0;i<answerWord.length;i++) {
-						if (answerWord[i] == guess) {
-							curWord[i] = guess;  
-					  };
-				 };
-				 guessesLeft -= 1;
-				 console.log("You guessed right!")
-				 console.log("Guesses remaining:" + " " + guessesLeft);
-         console.log(curWord);
-        
-        
+					   for (var i =0;i<answerWord.length;i++) {
+								if (answerWord[i] == guess) {
+									curWord[i] = guess;  
+							  };
+						 };
+						 guessesLeft -= 1;
+						 console.log("You guessed right!")
+						 console.log("Guesses remaining:" + " " + guessesLeft);
+						 var afterString = curWord.toString();
+		         console.log(afterString);
+		        
+		        
+
+				} else {
+							console.log('didnt match anything');
+							guessesLeft -= 1;
+		     			guessesSoFar.push(guess);
+		     			console.log("Guesses remaining:" + " " + guessesLeft);
+			 	    	console.log('Guess so far are:' + guessesSoFar);
+			 	      
+		    };
+
+		    if (guessesLeft == 0){
+		    	 console.log("You loss!")
+		  	   console.log('Guess so far are:' + guessesSoFar);  
+		  	  	  
+		    }
+
 
 		} else {
-					console.log('didnt match anything');
-     			guessesSoFar.push(guess);
-     			console.log("Guesses remaining:" + " " + guessesLeft);
-	 	    	console.log('Guess so far are:' + guessesSoFar);
-	 	      
-    };
 
-    if (guessesLeft == 0){
-    	 console.log("You loss!")
-  	   console.log('Guess so far are:' + guessesSoFar);  
-  	  /*startGame();*/
-  	  
-    }
+		  	 for (var i =0;i<answerWord.length;i++) {
+				   curWord.push('_');
+			   };
+/*
+     var function letterMatch() {}
+*/				if (curWord.indexOf(guess)>=0) {
+			  	console.log('You guessed this letter already!')
+			  } else if (answerWord.indexOf(guess)>=0) {
 
+					   for (var i =0;i<answerWord.length;i++) {
+								if (answerWord[i] == guess) {
+									curWord[i] = guess;  
+							  };
+						 };
+						 guessesLeft -= 1;
+						 console.log("You guessed right!")
+						 console.log("Guesses remaining:" + " " + guessesLeft);
+						 var afterString = curWord.toString();
+		         console.log(afterString);
+		        
+		        
 
+				} else {
+							console.log('didnt match anything');
+							guessesLeft -= 1;
+		     			guessesSoFar.push(guess);
+		     			console.log("Guesses remaining:" + " " + guessesLeft);
+			 	    	console.log('Guess so far are:' + guessesSoFar);
+			 	      
+		    };
 
+		    if (guessesLeft == 0){
+		    	 console.log("You loss!")
+		  	   console.log('Guess so far are:' + guessesSoFar);  
+		  	  	  
+		    }
+
+  
+     };
 	};
 
 };
