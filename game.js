08fb3e023bac2ace1ee7b8eed7fbf6a9aa386f86
Dante <lcdantechen@gmail.com>
2016-07-17 22:12:06 -0400

    update

--- game.js
+++ game.js
@@ -1,7 +1,49 @@
 exports.randomWords = [
 {
-	word: "mastur",
+	word: "masturdating",
 	meaning : "going out alone to a movie or a restaurant" 
 },
+
+{
+	word: "dudevorce",
+	meaning : "when two bros officially end their friendship"
+},
+
+{
+	word: "nonversation",
+	meaning : "a completely worthless conversation"
+},
+
+{
+	word: "gellfish", 
+	meaning : "an individual who continues talking on their phone so as to be rude or inconsiderate to other people"
+},
+
+{
+	word: "errorist",
+	meaning : "someone who repeateldly makes mistakes or is always wrong"
+},
+
+{
+	word: "askhole",
+	meaning : "someone who asks many stupid pointless questions"
+},
+
+{
+	word: "bedgasm",
+	meaning : "a feeling of euphoria experienced when climbing into bed at the end of a very long day"
+},
+
+{
+	word: "internest",
+	meaning : "the cocoon of blankets and pillows you gather around yourself whilst spending long periods of time on the internet"
+},
+
+{
+	word: "chairdrobe", 
+	meaning : "piling clothes on a chair in place of a closet or dresser"
+},
+
+
 
 ];
